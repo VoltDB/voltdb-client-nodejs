650580e55a28df5cd3fed3f5f18c657f69e366e2
Andrew Wilson <awilson@voltdb.com>
2012-01-23 14:23:36 -0500

    reformatting to 80 columns Removed some logging statements

--- voternoui.js
+++ voternoui.js
@@ -43,8 +43,30 @@ var options = cli.parse({
     voteCount : ['c', 'Number of votes to run', 'number', 1000]
 });
 
-var area_codes = [907, 205, 256, 334, 251, 870, 501, 479, 480, 602, 623, 928, 520, 341, 764, 628, 831, 925, 909, 562, 661, 510, 650, 949, 760, 415, 951, 209, 669, 408, 559, 626, 442, 530, 916, 627, 714, 707, 310, 323, 213, 424, 747, 818, 858, 935, 619, 805, 369, 720, 303, 970, 719, 860, 203, 959, 475, 202, 302, 689, 407, 239, 850, 727, 321, 754, 954, 927, 352, 863, 386, 904, 561, 772, 786, 305, 941, 813, 478, 770, 470, 404, 762, 706, 678, 912, 229, 808, 515, 319, 563, 641, 712, 208, 217, 872, 312, 773, 464, 708, 224, 847, 779, 815, 618, 309, 331, 630, 317, 765, 574, 260, 219, 812, 913, 785, 316, 620, 606, 859, 502, 270, 504, 985, 225, 318, 337, 774, 508, 339, 781, 857, 617, 978, 351, 413, 443, 410, 301, 240, 207, 517, 810, 278, 679, 313, 586, 947, 248, 734, 269, 989, 906, 616, 231, 612, 320, 651, 763, 952, 218, 507, 636, 660, 975, 816, 573, 314, 557, 417, 769, 601, 662, 228, 406, 336, 252, 984, 919, 980, 910, 828, 704, 701, 402, 308, 603, 908, 848, 732, 551, 201, 862, 973, 609, 856, 575, 957, 505, 775, 702, 315, 518, 646, 347, 212, 718, 516, 917, 845, 631, 716, 585, 607, 914, 216, 330, 234, 567, 419, 440, 380, 740, 614, 283, 513, 937, 918, 580, 405, 503, 541, 971, 814, 717, 570, 878, 835, 484, 610, 267, 215, 724, 412, 401, 843, 864, 803, 605, 423, 865, 931, 615, 901, 731, 254, 325, 713, 940, 817, 430, 903, 806, 737, 512, 361, 210, 979, 936, 409, 972, 469, 214, 682, 832, 281, 830, 956, 432, 915, 435, 801, 385, 434, 804, 757, 703, 571, 276, 236, 540, 802, 509, 360, 564, 206, 425, 253, 715, 920, 262, 414, 608, 304, 307];
-var voteCandidates = 'Edwina Burnam,Tabatha Gehling,Kelly Clauss,Jessie Alloway,Alana Bregman,Jessie Eichman,Allie Rogalski,Nita Coster,Kurt Walser,Ericka Dieter,Loraine NygrenTania Mattioli';
+var area_codes = [907, 205, 256, 334, 251, 870, 501, 479, 480, 602, 623, 928, 
+520, 341, 764, 628, 831, 925, 909, 562, 661, 510, 650, 949, 760, 415, 951, 209,
+ 669, 408, 559, 626, 442, 530, 916, 627, 714, 707, 310, 323, 213, 424, 747, 
+ 818, 858, 935, 619, 805, 369, 720, 303, 970, 719, 860, 203, 959, 475, 202, 
+ 302, 689, 407, 239, 850, 727, 321, 754, 954, 927, 352, 863, 386, 904, 561, 
+ 772, 786, 305, 941, 813, 478, 770, 470, 404, 762, 706, 678, 912, 229, 808, 
+ 515, 319, 563, 641, 712, 208, 217, 872, 312, 773, 464, 708, 224, 847, 779, 
+ 815, 618, 309, 331, 630, 317, 765, 574, 260, 219, 812, 913, 785, 316, 620, 
+ 606, 859, 502, 270, 504, 985, 225, 318, 337, 774, 508, 339, 781, 857, 617, 
+ 978, 351, 413, 443, 410, 301, 240, 207, 517, 810, 278, 679, 313, 586, 947, 
+ 248, 734, 269, 989, 906, 616, 231, 612, 320, 651, 763, 952, 218, 507, 636, 
+ 660, 975, 816, 573, 314, 557, 417, 769, 601, 662, 228, 406, 336, 252, 984, 
+ 919, 980, 910, 828, 704, 701, 402, 308, 603, 908, 848, 732, 551, 201, 862, 
+ 973, 609, 856, 575, 957, 505, 775, 702, 315, 518, 646, 347, 212, 718, 516, 
+ 917, 845, 631, 716, 585, 607, 914, 216, 330, 234, 567, 419, 440, 380, 740, 
+ 614, 283, 513, 937, 918, 580, 405, 503, 541, 971, 814, 717, 570, 878, 835, 
+ 484, 610, 267, 215, 724, 412, 401, 843, 864, 803, 605, 423, 865, 931, 615, 
+ 901, 731, 254, 325, 713, 940, 817, 430, 903, 806, 737, 512, 361, 210, 979, 
+ 936, 409, 972, 469, 214, 682, 832, 281, 830, 956, 432, 915, 435, 801, 385, 
+ 434, 804, 757, 703, 571, 276, 236, 540, 802, 509, 360, 564, 206, 425, 253, 
+ 715, 920, 262, 414, 608, 304, 307];
+var voteCandidates = 'Edwina Burnam,Tabatha Gehling,Kelly Clauss,' +
+'Jessie Alloway,Alana Bregman,Jessie Eichman,Allie Rogalski,Nita Coster,' + 
+'Kurt Walser,Ericka Dieter,Loraine NygrenTania Mattioli';
 
 
 function main() {
@@ -60,9 +82,9 @@ function main() {
     
     client = new VoltClient(configs);    
     client.connect(function startup(results) {
-        console.log('Node up');
         counter++;
         if ( counter == clusterNodes.length) {
+            console.log('Node up');
             voltInit();
         }
     }, function loginError(results) {
@@ -107,11 +129,11 @@ function voteResults(voteJob) {
 
         var msg = '';
         var longestString = 0;
-        console.log(results.status);
         var rows = results.table[0];
         for(var i = 0; i < rows.length; i++) {
             mytotalVotes += rows[i].TOTAL_VOTES;
-            msg += util.format("%s\t%s\t%d\n", rows[i].CONTESTANT_NAME, rows[i].CONTESTANT_NUMBER, rows[i].TOTAL_VOTES);
+            msg += util.format("%s\t%s\t%d\n", rows[i].CONTESTANT_NAME, 
+                rows[i].CONTESTANT_NUMBER, rows[i].TOTAL_VOTES);
         }
         msg += util.format("%d votes\n\n", mytotalVotes);
         console.log(msg);
@@ -134,13 +156,16 @@ function getCandidate() {
 }
 
 function getAreaCode() {
-    return area_codes[Math.floor(Math.random() * area_codes.length)] * 10000000 + Math.random() * 10000000;
+    return area_codes[Math.floor(Math.random() * area_codes.length)] * 10000000 
+        + Math.random() * 10000000;
 }
 
 function getSteps() {
     var voltTestChain = [];
     voltTestChain.push(voteResults);
-    voltTestChain.push(voteResultsOften);
+    // Not called because the query does a table scan and is not 
+    // representative of VoltDB's performance
+    //voltTestChain.push(voteResultsOften); 
     voltTestChain.push(voteOften);
     voltTestChain.push(voteResults);
     voltTestChain.push(voteEnd);
@@ -178,7 +203,9 @@ function voteResultsLoop(voteJob) {
                 //console.log('writes left: ', voteJob.voteCount-index);
                 if(index < voteJob.voteCount) {
                     if ( index % 5000 == 0 ) {
-                        console.log('Executed ', index, ' queries in ', (new Date().getTime()) - chunkTime, 'ms ', util.inspect(process.memoryUsage()));
+                        console.log('Executed ', index, ' queries in ', 
+                            (new Date().getTime()) - chunkTime, 'ms ', 
+                            util.inspect(process.memoryUsage()));
                         chunkTime = new Date().getTime();
                     }
                     index++;
@@ -189,7 +216,8 @@ function voteResultsLoop(voteJob) {
                 //console.log('write done');
             });
         } else {
-            console.log(readyToWriteCounter++, 'Index is: ', index, ' and ', voteJob.voteCount);
+            console.log(readyToWriteCounter++, 'Index is: ', index, ' and ', 
+                voteJob.voteCount);
         }
     };
     process.nextTick(innerResultsLoop);
@@ -223,14 +251,17 @@ function voteInsertLoop(voteJob) {
                 
                 if(index < voteJob.voteCount) {
                     if ( index % 5000 == 0 ) {
-                        console.log('Executed ', index, ' votes in ', (new Date().getTime()) - chunkTime, 'ms ', util.inspect(process.memoryUsage()));
+                        console.log('Executed ', index, ' votes in ', 
+                        (new Date().getTime()) - chunkTime, 'ms ', 
+                        util.inspect(process.memoryUsage()));
                         chunkTime = new Date().getTime();
                     }
 
                     index++;
                     process.nextTick(innerLoop);
                 } else {
-                    console.log(readyToWriteCounter++, 'Time to stop voting: ', index);
+                    console.log(readyToWriteCounter++, 'Time to stop voting: ',
+                         index);
                 }
             });
         } else {
